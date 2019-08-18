1e2b297a80c02cb9b7e1eece23868731e66f8a7c
Jose-AlvarezdeLugo <alvarezdelugo.jose@gmail.com>
2019-08-18 12:55:05 -0400

    [LOCAL][FEATURE] Add separate loop function

--- my_shell.c
+++ my_shell.c
@@ -8,34 +8,17 @@
   */
 int main(void)
 {
-	char *buff = NULL, **tokens = NULL, *delim = " ";
-	ssize_t lgetline = 0, buffsize = 1024;
-	int i = 0;
+	/*
+	if (argc > 2)
+		return (-1); */
+	/* check if argv[1] is a file */
 
-	while (1)
-	{
-		/* write prompt */
-		/* isatty should be checked here (if true, print prompt) */
-		write(STDOUT_FILENO, "$ ", 2);
-		buff = do_mem(buffsize, NULL);
+	/* signal handler */
 
-		/* read command, getline and write output until it fails */
-		while ((lgetline = _getline(&buff, buffsize, STDIN_FILENO)) < 0)
-		{
-			do_mem(0, buff);
-			do_exit(2, "", -1);
-		}
+	/* read, tokenize, execute loop */
+	main_loop();
 
-		/* check for exit command */
-		if ((_strcmp(buff, "exit\n")) == 0)
-			do_exit(2, "", 100);
-		tokens = _strtok(buff, delim);
-		for (i = 0; tokens[i]; i++)
-		{
-			write(STDOUT_FILENO, tokens[i], _strlen(tokens[i]));
-			write(STDOUT_FILENO, " ", 1);
-		}
-	}
-	do_mem(0, buff);
+	/* clean up */
+
 	return (0);
 }
