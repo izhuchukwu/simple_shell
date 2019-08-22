ac8fd7854d8f0fa1341acbf97633b3c51c3cb8de
Jose-AlvarezdeLugo <alvarezdelugo.jose@gmail.com>
2019-08-22 16:28:12 -0400

    [LOCAL][FEATURE] Add cd builtin function

---
+++ builtin_ops.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * cd_builtin - executes cd function, changes directory
+ * @tokens: command input into prompt tokenized
+ */
+void cd_builtin(char **tokens)
+{
+    char *HOME, *lastdir;
+
+    lastdir = do_mem(100, NULL);
+    if (tokens[1] == "-")
+    {
+        /* go to previous directory */
+        chdir(lastdir);
+    }
+    else if (tokens[1])
+    {
+        /* change directory to tokens[1] */
+        getcwd(lastdir, 100);
+        chdir(tokens[1]);
+    }
+    else
+    {
+        /* change to home directory */
+        getcwd(lastdir, 100);
+        chdir(get_envval(environ, "HOME"));
+    }
+}
