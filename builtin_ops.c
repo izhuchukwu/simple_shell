c3dda435780364b44474ec013a453d58eff897a1
william <818@holbertonschool.com>
2019-08-22 21:25:00 +0000

    [LOCAL][BUGFIX] Make betty compliant

--- builtin_ops.c
+++ builtin_ops.c
@@ -6,24 +6,25 @@
  */
 void cd_builtin(char **tokens)
 {
-    char *HOME = NULL, *lastdir = NULL;
+	char *HOME = NULL, *lastdir = NULL;
 
-    lastdir = do_mem(100, NULL);
-    if (_strcmp(tokens[1], "-") == 0)
-    {
-        /* go to previous directory */
-        chdir(lastdir);
-    }
-    else if (tokens[1])
-    {
-        /* change directory to tokens[1] */
-        getcwd(lastdir, 100);
-        chdir(tokens[1]);
-    }
-    else
-    {
-        /* change to home directory */
-        getcwd(lastdir, 100);
-        chdir(get_env_val(environ, "HOME"));
-    }
+	(void)HOME;
+	lastdir = do_mem(100, NULL);
+	if (_strcmp(tokens[1], "-") == 0)
+	{
+		/* go to previous directory */
+		chdir(lastdir);
+	}
+	else if (tokens[1])
+	{
+		/* change directory to tokens[1] */
+		getcwd(lastdir, 100);
+		chdir(tokens[1]);
+	}
+	else
+	{
+		/* change to home directory */
+		getcwd(lastdir, 100);
+		chdir(get_env_val(environ, "HOME"));
+	}
 }
