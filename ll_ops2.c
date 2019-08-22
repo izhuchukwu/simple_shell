77aec2949c13e95fad53e845cbc1550f97ccd8e0
william <818@holbertonschool.com>
2019-08-22 21:51:02 +0000

    [LOCAL][BUGFIX] Make betty compliant

--- ll_ops2.c
+++ ll_ops2.c
@@ -1,23 +1,35 @@
 #include "shell.h"
 
+/**
+ * arrayify - copy a char * linked list into a char **
+ * @head: the head of the linked list
+ * Return: a malloc'd char **
+ */
 char **arrayify(list_s *head)
 {
 	char **ret = NULL;
 	char *tmp = NULL;
+	char *tmp2 = NULL;
 	int len = 0, i = 0;
 
 	len = list_len(head);
 	ret = do_mem(sizeof(char *) * (len + 1), NULL);
 	for (i = 0; i < len; i++)
 	{
-		tmp = do_mem(sizeof(char) * (_strlen(get_node_at_index(head, i)->ptr) + 1), NULL);
-		tmp = _strcat(tmp, get_node_at_index(head, i)->ptr);
+		tmp2 = get_node_at_index(head, i)->ptr;
+		tmp = do_mem(sizeof(char) * (_strlen(tmp2) + 1), NULL);
+		tmp = _strcat(tmp, tmp2);
 		ret[i] = tmp;
 	}
 	ret[i] = NULL;
 	return (ret);
 }
 
+/**
+ * listify - copy a char ** array into a malloc'd char * linked list
+ * @arr: the char **
+ * Return: a malloc'd char * linked list
+ */
 list_s *listify(char **arr)
 {
 	list_s *ret = NULL;
