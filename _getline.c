9492903a55af13957dbbac6c6e05a74d3d16612c
william <818@holbertonschool.com>
2019-08-27 15:28:24 +0000

    [LOCAL][FEATURE] Make getline with 4096 byte buffer

--- _getline.c
+++ _getline.c
@@ -30,12 +30,57 @@ char *_getline2()
  * @stream: the stream to read from
  * Return: the number of bytes read
  */
-ssize_t _getline(char **lineptr, size_t n, int stream)
+ssize_t _getline(char *lineptr, int stream)
 {
-	ssize_t lread;
+        static int input[4096];
+	static int filled;
+	int newline_index = -1, i = 0, tmp = 2;
+	ssize_t ret = 0;
 
-	/* reads std input and checks if it fails */
-	lread = read(stream, *lineptr, n);
+	/* if the buffer is empty, fill it */
+	if (!filled)
+	{
+		read(stream, input, 4096);
+		filled = 4096;
+	}
 
-	return (lread);
+
+	/* if the buffer contains \n or EOF */
+	newline_index = has_newline(input);
+	if (newline_index != -1)
+	{
+		for(i = 0; i <= newline_index; i++)
+		{
+			lineptr[i] = input[i];
+		}
+		ret = newline_index + 1;
+
+		/* Shift any remaining chars to the left */
+		shiftbuffer(input, newline_index, filled);
+		filled = filled - ret;
+
+		return (ret);
+	}
+	/* if the buffer doesn't contain \n or EOF */
+	else
+	{
+		/* if the buffer is full then read until \n or EOF */
+		if (filled == 4096)
+		{
+			/* should always fill buffer with \n or EOF at end*/
+			while(tmp != -1 && tmp != '\n')
+				tmp = read(stream, &tmp, 1);
+			input[4095] = tmp;
+			return (_getline(lineptr, stream));
+		}
+		/* if the buffer isn't full, then fill it and try again. */
+		else
+		{
+			read(stream, input + filled, 4096 - filled);
+			return (_getline(lineptr, stream));
+		}
+
+	}
+
+	return (-1);
 }
